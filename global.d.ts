eaf9a19512c665353057ce45c6371ccf107e71a9
Revenity <87530663+aquapi@users.noreply.github.com>
2023-02-18 22:25:45 +0700

    Update global

--- global.d.ts
+++ global.d.ts
@@ -1,8 +1,11 @@
 /// <reference no-default-lib="true" />
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+/// <reference lib="esnext" />
 
-/**
- * Parsed parameter
- */
 declare interface Window {
+    /**
+     * Parsed parameter
+     */
     readonly params: Record<string, string>;
 }
