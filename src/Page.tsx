a2a147630d53ff996abe1328b31c294248796b12
Revenity <87530663+aquapi@users.noreply.github.com>
2023-02-25 18:59:02 +0700

    Update page router

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,21 +1,17 @@
 import { template as defaultTemplate } from "./template";
-
 import esbuild from "esbuild";
-
-import Bun from "bun";
-
-import { Router } from "@stricjs/router";
-import { stream } from "@stricjs/utils";
-import { App } from "@stricjs/core";
-
 import ReactDOM from "react-dom/server";
-
 import Path from "path/posix";
 
-import fs, { existsSync } from "fs";
+import { existsSync, rm as removePath } from "fs";
 import { promisify } from "util";
+import { mkdir, appendFile } from "fs/promises";
+
 import { Template } from "./types";
 import { hashContent } from "./hash";
+
+import { stream } from "@stricjs/utils";
+import { PageRouter as PRouter } from "@stricjs/pages";
 
 type RouteList = {
     ssr?: boolean;
@@ -25,9 +21,7 @@ type RouteList = {
     id: string,
 }[];
 
-const rm = promisify(fs.rm);
-const mkdir = promisify(fs.mkdir);
-const appendFile = promisify(fs.appendFile);
+const rm = promisify(removePath);
 
 async function build(files: RouteList, outdir: string, dev: boolean, root: string, other: esbuild.BuildOptions) {
     const tmps = files.map(v => Path.join(outdir, Path.basename(v.source).split(".")[0] + `.${v.id}.tsx`));
@@ -67,48 +61,17 @@ async function build(files: RouteList, outdir: string, dev: boolean, root: strin
     await Promise.all(tmps.map(f => rm(f)));
 };
 
-export class PageRouter<T = any> {
-    readonly src: string;
-    readonly out: string;
-    readonly dev: boolean;
-    readonly root: string;
-
+export class PageRouter<T = any> extends PRouter<T> {
     private routesData: RouteList;
-
-    private readonly app: App<T>;
-    readonly router: Router<T>;
     readonly template: Template;
 
     /**
      * Create a page with a template 
      */
     constructor(private readonly build: esbuild.BuildOptions = {}) {
-        this.root = Path.resolve();
-        this.src = "src";
-        this.out = "out";
-        this.dev = Bun.env.NODE_ENV !== "production";
-
+        super();
         this.template = defaultTemplate;
-
         this.routesData = [];
-
-        this.app = new App();
-        this.router = new Router<T>();
-    }
-
-    set(field: "src" | "out" | "root", value: string): this;
-    set(field: "dev", value: boolean): this;
-    set(field: "router", value: Router<T>): this;
-    set(field: "template", value: Template): this;
-    /**
-     * Set private fields 
-     * @param field 
-     * @param value 
-     */
-    set(field: string, value: any) {
-        this[field] = value;
-
-        return this;
     }
 
     /**
@@ -150,7 +113,7 @@ export class PageRouter<T = any> {
      */
     async load(streamOpts?: BlobPropertyBag & ResponseInit) {
         const outDir = Path.join(this.root, this.out);
-        if (fs.existsSync(outDir))
+        if (existsSync(outDir))
             await rm(outDir, { recursive: true });
         await mkdir(outDir);
 
@@ -219,20 +182,5 @@ export class PageRouter<T = any> {
         this.app.development = this.dev;
 
         return this;
-    }
-
-    /**
-     * Serve the app
-     */
-    async serve() {
-        await this.load();
-        return Bun.serve(this.app);
-    }
-
-    /**
-     * Return the fetch handler
-     */
-    fetch(): (request: Request<any>, server: Bun.Server) => Promise<Response> {
-        return this.app.fetch.bind(this.app);
     }
 }
